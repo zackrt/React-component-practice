3fea4da46b0cfcf182f2d5a68999d5a0a54924c3
Zack Toth <zacharyrtoth@gmail.com>
2018-03-24 09:46:39 -0700

    added to status-section.js file

--- hotandcold/src/components/info-section.js
+++ hotandcold/src/components/info-section.js
@@ -1,1 +1,18 @@
 import React from 'react';
+
+import './info-section.css';
+
+export default function InfoSection(props) {
+    return (
+        <section id="what" tabIndex="-1">
+            <h2>What do I do?</h2>
+            <p>This is a Hot or Cold Number Guessing Game. The game goes like this:</p>
+                <ol>
+                    <li>I pick a <strong>random secret number</strong> between 1 to 100 and keep it hidden.</li>
+                    <li>You need to <strong>guess</strong>until you can find the hidden secret number.</li>
+                    <li>You will <strong>get feedback</strong> on how close ("hot") or far ("cold") your guess is.</li>
+                </ol>
+            <p>If you're using a screen reader, you can use the visually hidden "Hear state of game" link in the navigation bar to help you</p>
+        </section>
+    );
+}
