54e0a3b671290535dd29907346656d033bdc8229
Zack Toth <zacharyrtoth@gmail.com>
2018-03-24 09:58:19 -0700

    added to top-nav.js file

---
+++ hotandcold/src/components/top-nav.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './top-nav.css';
+
+export default function TopNav(props){
+    return (
+        <nav>
+            <ul className="clearfix">
+                <li>
+                    <a 
+                        href="#what"
+                        className="what"
+                        aria-label="How to play"
+                    >
+                    What?
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="#feedback"
+                        className="new"
+                        aria-label="Start a new game"
+                        onClick={() => props.onRestartGame()}
+                    >
+                    + New game
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="#get-status"
+                        className="visuallyhidden focusable status-link"
+                        onClick={() => props.onGenerateAuralUpdate()}
+                    >
+                        Hear state of game
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
