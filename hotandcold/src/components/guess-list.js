8fc7d94abfd602c40251d5ebde29b2d1a3640613
Zack Toth <zacharyrtoth@gmail.com>
2018-03-24 09:12:35 -0700

    added to js files

--- hotandcold/src/components/guess-list.js
+++ hotandcold/src/components/guess-list.js
@@ -1,1 +1,16 @@
 import React from 'react'; 
+
+import './guess-list.css';
+
+export default function GuessList(props) {
+    const guesses = props.guesses.map((guess, index) =>(
+        <li key={index}>
+            {guess}
+        </li>
+    ));
+    return (
+        <ul id="guessList" className="guessBox clearfix">
+        {guesses}
+        </ul>
+    );
+}
